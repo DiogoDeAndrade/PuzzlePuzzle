573297682d1045325e15c5e22b977aebd019140c
Diogo de Andrade <diogo.g.andrade@gmail.com>
2025-03-01 18:47:29 +0000

    Automatic build system integration

--- upload.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-:: Set the version variable
-set version=1.0.1
-
-:: Use the version variable in the command
-c:\opt\butler\butler.exe push PuzzlePuzzle_v%version%.zip diogoandrade/puzzlepuzzle:windows --userversion %version%
-c:\opt\butler\butler.exe push PuzzlePuzzleWeb_v%version%.zip diogoandrade/puzzlepuzzle:html5 --userversion %version%
